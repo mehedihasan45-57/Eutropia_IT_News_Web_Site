1edaa2ee1d3d0c8f6d76a4736f4fea51eb2e6b07
Mehedi Hasan <93275914+mehedihasan45-57@users.noreply.github.com>
2023-02-18 21:33:17 +0600

    Add files via upload

---
+++ mynews/News/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import News
+from .forms import NewsForm
+
+# Create your views here.
+def News(request):
+    return HttpResponse()
+def news_list(request):
+    news = News.objects.all()
+    return render(request, 'news/news_list.html', {'news': news})
+
+def home(request):
+    news = News.objects.all()
+    return render(request, 'base.html', {'home': home})
+
+def news_create(request):
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm()
+    return render(request, 'news/news_form.html', {'form': form})
+
+
+def news_update(request, pk):
+    news = News.objects.get(pk=pk)
+    if request.method == 'POST':
+        form = NewsForm(request.POST, instance=news)
+        if form.is_valid():
+            form.save()
+            return redirect('news_list')
+    else:
+        form = NewsForm(instance=news)
+    return render(request, 'news/news_form.html', {'form': form})
+
+def news_delete(request, pk):
+    News.objects.get(pk=pk).delete()
+    return redirect('news_list')
